c6c30c7949969f0c223e29f02857a2ca83e2e103
rifandani <rifandani@users.noreply.github.com>
2021-07-01 07:49:40 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.269a6d77d1d03a74d1c2c96709f8cbdd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "955c8b4614d38f6023341c3d74965729",
+    "url": "https://rifandani.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "618326591a15fe138289",
+    "url": "https://rifandani.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "ee38f3e54cc1313715d3",
+    "url": "https://rifandani.github.io/task-management-frontend/static/js/2.455303b1.chunk.js"
+  },
+  {
+    "revision": "618326591a15fe138289",
+    "url": "https://rifandani.github.io/task-management-frontend/static/js/main.eac61bbd.chunk.js"
+  },
+  {
+    "revision": "c8e072d7f27fb6ada4e3",
+    "url": "https://rifandani.github.io/task-management-frontend/static/js/runtime~main.d3e1e7f6.js"
+  }
+]);
